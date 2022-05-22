8245bed72b448565cef63977c346f5f89b78d833
Miguel <mad_morla@hotmail.com>
2022-05-22 21:37:59 +0200

    Changing the pickup code from PlayerController to PickableObject script

--- Assets/Scripts/Interaction/PickableObject.cs
+++ Assets/Scripts/Interaction/PickableObject.cs
@@ -10,23 +10,29 @@ public class PickableObject : MonoBehaviour, IRaycastable
     public Vector3 InitialPos { get => initialPos; }
     public Quaternion InitialRot { get => initialRot; }
 
+    private bool pickingFinished = true;
+
     public bool HandleRaycast(PlayerController callingController)
     {
-        //TODO: Fix This
-        // Get all to this script, interaction, etc...
+        // When you click left mouse button then try to pickup a object
         if (Input.GetMouseButtonDown(0))
         {
+            // If don't have an object picked, then pickup
             if (!callingController.RightHandObject)
             {
                 SetInitialState();
-                callingController.PickupObject(this);
+                PickupObject(this, callingController);
             }
             else
             {
-                callingController.LeaveObject();
+                // If there is an object picked, then leave it
+                LeaveObject(callingController);
+
+                // If you want to pick another object, but you have another picked,
+                // then leave the picked one
                 if (callingController.RightHandObject != this)
                 {
-                    callingController.LeaveObject();
+                    LeaveObject(callingController);
                 }
             }
         }
@@ -40,4 +46,75 @@ public class PickableObject : MonoBehaviour, IRaycastable
         initialRot = this.transform.rotation;
     }
 
+
+    //----------------------------------
+    // Pickup Objects Methods
+
+    public void PickupObject(PickableObject pickableObj, PlayerController callingController)
+    {
+        if (pickingFinished)
+        {
+            StartCoroutine(PickupObjectCoroutine(pickableObj, callingController));
+        }
+    }
+
+    public void LeaveObject(PlayerController callingController)
+    {
+        if (pickingFinished)
+        {
+            StartCoroutine(LeaveObjectCoroutine(callingController));
+        }
+    }
+
+    private IEnumerator PickupObjectCoroutine(PickableObject pickableObj, PlayerController callingController)
+    {
+        print("PickingObject " + pickableObj.name);
+        pickingFinished = false;
+        callingController.RightHandObject = pickableObj;
+
+        Transform initT = callingController.RightHandObject.transform;
+        Transform targetT = callingController.TargetRightHand;
+
+        yield return InterpolateObject(initT,
+                        initT.position, targetT.position,
+                        initT.rotation, targetT.rotation,
+                        callingController.RightHandSpeed);
+        pickingFinished = true;
+    }
+
+    private IEnumerator LeaveObjectCoroutine(PlayerController callingController)
+    {
+        print("LeavingObject " + callingController.RightHandObject.name);
+        pickingFinished = false;
+
+        Transform initT = callingController.RightHandObject.transform;
+
+        yield return InterpolateObject(initT,
+                        initT.position, callingController.RightHandObject.InitialPos,
+                        initT.rotation, callingController.RightHandObject.InitialRot,
+                        callingController.RightHandSpeed);
+
+        pickingFinished = true;
+        callingController.RightHandObject = null;
+    }
+
+    private IEnumerator InterpolateObject(Transform objT, Vector3 initPos, Vector3 targetPos, Quaternion initRot, Quaternion targetRot, float speed)
+    {
+        float t = 0;
+        while (!HasReachedDestination(objT.position, targetPos))
+        {
+            t += Mathf.Clamp01(Time.deltaTime * speed);
+            objT.position = Vector3.Lerp(initPos, targetPos, t);
+            objT.rotation = Quaternion.Lerp(initRot, targetRot, t);
+
+            yield return null;
+        }
+    }
+
+    private bool HasReachedDestination(Vector3 currentPos, Vector3 targetPos)
+    {
+        return Mathf.Approximately(Vector3.Distance(currentPos, targetPos), 0);
+    }
+
+    //----------------------------------
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,16 +10,14 @@ public class PlayerController : MonoBehaviour
 
     [Range(0.5f, 3)]
     [SerializeField] private float rightHandSpeed = 2f;
+    public float RightHandSpeed { get => rightHandSpeed; }
 
     private Camera cam;
 
     // Take Object with right hand
     [SerializeField] private PickableObject rightHandObject;
-    public PickableObject RightHandObject { get => rightHandObject; }
+    public PickableObject RightHandObject { get => rightHandObject; set => rightHandObject = value; }
 
-    private Vector3 rightHandObject_Initialposition;
-    private Quaternion rightHandObject_Initialrotation;
-    private bool pickingFinished = true;
 
 
     //----------------------------------
@@ -43,12 +41,15 @@ public class PlayerController : MonoBehaviour
 
     private bool InteractWithObjects()
     {
+        // Cast a ray from the mouse in camera direction and get the first hit
         RaycastHit hit;
         if (Physics.Raycast(GetMouseRay(), out hit))
         {
+            // Get all the IRaycastable components from the hit object
             IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
             foreach (IRaycastable raycastable in raycastables)
             {
+                // Execute the handle function implemented for that IRaycastable object
                 if (raycastable.HandleRaycast(this))
                 {
                     return true;
@@ -62,70 +63,5 @@ public class PlayerController : MonoBehaviour
     {
         return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
-
-
-    //----------------------------------
-    // Pickup Objects Methods
-
-    public void PickupObject(PickableObject pickableObj)
-    {
-        if (!rightHandObject && pickingFinished)
-        {
-            StartCoroutine(PickupObjectCoroutine(pickableObj));
-        }
-    }
-
-    public void LeaveObject()
-    {
-        if (rightHandObject && pickingFinished)
-        {
-            StartCoroutine(LeaveObjectCoroutine());
-        }
-    }
-
-    private IEnumerator PickupObjectCoroutine(PickableObject pickableObj)
-    {
-        print("PickingObject " + pickableObj.name);
-        pickingFinished = false;
-        rightHandObject = pickableObj;
-
-        yield return InterpolateObject(rightHandObject.transform,
-                        rightHandObject.transform.position, targetRightHand.position,
-                        rightHandObject.transform.rotation, targetRightHand.rotation);
-        pickingFinished = true;
-
-    }
-
-    private IEnumerator LeaveObjectCoroutine()
-    {
-        print("LeavingObject " + rightHandObject.name);
-        pickingFinished = false;
-
-        yield return InterpolateObject(rightHandObject.transform,
-                        rightHandObject.transform.position, rightHandObject.InitialPos,
-                        rightHandObject.transform.rotation, rightHandObject.InitialRot);
-        pickingFinished = true;
-        rightHandObject = null;
-    }
-
-    private IEnumerator InterpolateObject(Transform objT, Vector3 initPos, Vector3 targetPos, Quaternion initRot, Quaternion targetRot)
-    {
-        float t = 0;
-        while (!HasReachedDestination(objT.position, targetPos))
-        {
-            t += Time.deltaTime * rightHandSpeed;
-            objT.position = Vector3.Lerp(initPos, targetPos, t);
-            objT.rotation = Quaternion.Lerp(initRot, targetRot, t);
-
-            yield return null;
-        }
-    }
-
-    private bool HasReachedDestination(Vector3 currentPos, Vector3 targetPos)
-    {
-        return Mathf.Approximately(Vector3.Distance(currentPos, targetPos), 0);
-    }
-
-    //----------------------------------
 
 }
