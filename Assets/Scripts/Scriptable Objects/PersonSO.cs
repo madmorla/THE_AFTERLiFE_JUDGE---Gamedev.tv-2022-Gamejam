31dc0a6a0c21c95d15b7886a37c6c7d4f04bbe15
Macks404 <maxj0852@hotmail.com>
2022-05-22 09:51:49 +0100

    Added more stuff to the scriptable object

--- Assets/Scripts/Scriptable Objects/PersonSO.cs
+++ Assets/Scripts/Scriptable Objects/PersonSO.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Person", menuName = "SO/New Person")]
 public class PersonSO : ScriptableObject
 {
-    [Header("Basic Info")]
+    [Header("Info")]
     [SerializeField] public Image image;
-    [SerializeField] public string name;
+    [SerializeField] public string fullname;
     [SerializeField] public int age;
     [SerializeField][TextArea] public string description;
     [SerializeField][TextArea] public string howDied;
+
+    [SerializeField] public string[] righteousActs;
+    [SerializeField] public string[] sinfulActs;
+
+    [SerializeField] public string[] familyMembers;
+
+
+
+
+
 }
